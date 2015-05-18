5fff88b8a4170e4c02a7d92429501c2064fc65e2
Kosuke Hata <hata.kosuke@gmail.com>
2015-05-17 19:09:18 -0700

    done

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,25 +3,26 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 	
-	public float ballInitialVelocity = 600.0f;
-
+	public float ballInitialVelocity = 600f;
+	
+	
 	private Rigidbody rb;
 	private bool ballInPlay;
-
-	// Use this for before Start ()
+	
 	void Awake () {
+		
 		rb = GetComponent<Rigidbody>();
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-		if(Input.GetButtonDown("Fire1") && ballInPlay == false) {
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
 			transform.parent = null;
 			ballInPlay = true;
 			rb.isKinematic = false;
 			rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
 		}
-	
 	}
 }
---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadZone : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider collider)
+	{
+		GM.instance.LoseLife();
+	}
+}
